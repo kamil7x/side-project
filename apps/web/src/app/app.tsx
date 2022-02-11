60eb740aeee33d3c8946ae9e844f826d54bde377
Kamil Trusiak <k.trusiak@outlook.com>
2022-02-11 08:37:07 +0100

    wip

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,12 +1,28 @@
+import {
+  Module5x7Matrix,
+  Module7Segments,
+  ReactDigitalDisplay,
+} from 'react-digital-display';
+
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
+
+const TEXT = '9BB901';
+const MODULE = Module7Segments;
+const SIZE = 6;
+// const HEIGHT = 160;
+const HEIGHT = undefined;
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="web" />
-      <div />
-    </>
+    <div className={styles.container}>
+      <ReactDigitalDisplay
+        text={TEXT}
+        module={Module5x7Matrix}
+        size={SIZE}
+        height={HEIGHT}
+        unknownCharacterMode="empty"
+      />
+    </div>
   );
 }
 
--- libs/react-digital-display/src/index.ts
+++ libs/react-digital-display/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/module-5x7-matrix/module-5x7-matrix';
 export * from './lib/module-7-segments/module-7-segments';
 export * from './lib/react-digital-display';
 export * from './lib/types';
